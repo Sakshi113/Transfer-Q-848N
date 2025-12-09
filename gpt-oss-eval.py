5f4d79d825b0988037b4d49a3948aa3017f92192
furrrow <jianyu234@gmail.com>
2025-12-08 19:57:25 -0500

    added gpt-oss-eval.py

---
+++ gpt-oss-eval.py
@@ -0,0 +1,122 @@
+from tqdm import tqdm
+import json
+import random
+import time
+import lmstudio as lms
+import re
+
+def get_prompt(question: str, ans_a: str, ans_b: str) -> str:
+
+    user_prompt = f"""[Question]
+        {question}
+
+        [The Start of Assistant 1's Answer]
+        {ans_a}
+
+        [The End of Assistant 1's Answer]
+
+        [The Start of Assistant 2's Answer]
+        {ans_b}
+
+        [The End of Assistant 2's Answer]"""
+    return user_prompt
+
+
+def get_response(database, idx):
+    idx = str(idx)
+    line = database[idx]['response']
+    if type(line) == dict:
+        last_response_key = list(database[idx]['response'])[-1]
+        last_response = database[idx]['response'][last_response_key]
+        last_response = last_response['worker']
+    else:
+        last_response = database[idx]['response']
+    return last_response
+
+
+if __name__ == "__main__":
+    """
+    I am using a different virtual environment with LM studio for this script
+    """
+
+    system_begin = """[System]
+        You are a precise assistant for checking the quality of the answer. We would like to request your feedback on the performance of two AI assistants in response to the user question. Please rate the level of detail of their responses. Your evaluation should consider factors such as the relevance, accuracy, depth, creativity, and level of detail of the response. Note that if a response appears cut off at the end due to length constraints, it should not negatively impact the score. Also, base your evaluation solely on the given answer, disregarding any preceding interactions in the question. Each assistant receives an overall score on a scale of 1 to 10, where a higher score indicates better overall performance."""
+    # System_end = "[System] Please first output a single line containing only two values indicating the scores for Assistant 1 and 2, respectively. The two scores are separated by a space. In the subsequent line, please provide a comprehensive explanation of your evaluation, avoiding any potential bias and ensuring that the order in which the responses were presented does not affect your judgment."
+    System_end = ("[System] "
+                  "Please output a single line containing only two values indicating the scores for Assistant 1 and 2, "
+                  "respectively. The two scores are separated by a space. ")
+    file1 = "run_outs/safenlp_direct_0.jsonl"
+    file2 = "run_outs/safenlp_llm_only_direct_0.jsonl"
+
+    with open(file1, "r") as file:
+        db1 = json.load(file)
+    with open(file2, "r") as file:
+        db2 = json.load(file)
+
+    num_eval = 50
+
+    model_name = "openai/gpt-oss-safeguard-20b"
+    start = time.time()
+    model = lms.llm(model_name)
+    elapsed = time.time() - start
+    print(f"model loaded in {elapsed:.3f}")
+
+    evaluations = []
+    win = tie = lose = 0
+    key_list = list(db1.keys())[1:]
+    for idx in tqdm(key_list):
+        prompt = db1[idx]["prompt"]
+        # verify the prompts line up:
+        assert (db1[idx]['prompt'] == db2[idx]['prompt'])
+        response_red = get_response(db1, idx)
+        response_blue = get_response(db2, idx)
+
+        side = random.randint(0, 1)
+        if side == 0:
+            user_prompt = get_prompt(prompt, response_red, response_blue)
+        else:
+            user_prompt = get_prompt(prompt, response_blue, response_red)
+
+        chat = lms.Chat(system_begin + user_prompt + System_end)
+        chat.add_user_message(user_prompt)
+        output = model.respond(chat).content
+
+        try:
+            score1, score2 = map(float, re.split(r"final<\|message\|>", output)[-1].split(" "))
+        except Exception:
+            print(output)
+            assert True
+            score1, score2 = 0, 0
+
+        if side == 1:
+            score1, score2 = score2, score1
+
+        evaluations.append(
+            {
+                "prompt": prompt,
+                "red_answer": response_red,
+                "blue_answer": response_blue,
+                "red_score": score1,
+                "blue_score": score2,
+                "result": output,
+            },
+        )
+
+        win += score1 > score2
+        tie += score1 == score2
+        lose += score1 < score2
+
+        print(win, tie, lose)
+
+        if int(idx) >= num_eval:
+            break
+
+    result = {
+        "run_name_red": file1,
+        "run_name_blue": file2,
+        "win": win,
+        "tie": tie,
+        "lose": lose,
+        "evaluations": evaluations,
+    }
+    
--- measure_reward.py
+++ measure_reward.py
@@ -11,6 +11,9 @@ torch.manual_seed(42)
 
 
 def main(args):
+    with open(args.out_file, "r") as out_f:
+        database = json.load(out_f)
+
     start = time.time()
     model_name = args.model_name
     print(f"Loading model {model_name} ...")
@@ -21,23 +24,23 @@ def main(args):
     rm_model = rm_model.to(args.device)
     rm_model.eval()
     print(f"model loaded in : {time.time() - start :.2f}")
-    with open(args.out_file, "r") as out_f:
-        database = json.load(out_f)
+
 
     rm_scores = []
     num_skip = 0
     for line in tqdm(database):
         if line == "run_configs":
             continue
-        # outp = extract_out(database, line)
-        last_response_key = list(database[line]['response'])[-1]
-        last_response = database[line]['response'][last_response_key]
-        if type(last_response) == dict:
+        if type(database[line]['response']) == dict:
+            last_response_key = list(database[line]['response'])[-1]
+            last_response = database[line]['response'][last_response_key]
             last_response = last_response['worker']
+        else:
+            last_response = database[line]['response']
         combined_out = database[line]['prompt'] + last_response
         # get rm_score
         tokens = tokenizer(combined_out, return_tensors="pt", padding=True).input_ids.to(args.device)
-        print(f"{tokens.shape=}")
+        # print(f"{tokens.shape=}")
         if tokens.shape[1] >= 1334: return None
         rm_out = rm_model(tokens)
         rm_score = rm_out.logits.flatten().item()
@@ -50,6 +53,9 @@ def main(args):
             continue
         else: rm_scores.append(rm_score)
 
+        if len(rm_scores) >= args.num_entries:
+            break
+
     print(f"{np.mean(rm_scores)=}")
     print(f"{num_skip=}")
     return np.mean(rm_scores)
@@ -57,8 +63,9 @@ def main(args):
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="usvsnsp/pythia-6.9b-rm-full-hh-rlhf")
-    parser.add_argument("--out_file", type=str, default="run_outs/backup_out_collab_0.jsonl")
+    parser.add_argument("--out_file", type=str, default="run_outs/llama_llm_only_direct_0.jsonl")
     parser.add_argument("--device", type=str, default="cuda:0")
+    parser.add_argument("--num_entries", type=int, default=50)
 
     args = parser.parse_args()
 
